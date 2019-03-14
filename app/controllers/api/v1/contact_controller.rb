3be76c5c96de0bcd20398454eaea6b6e3ec45a79
github username <suoarik@gmail.com>
2019-03-14 19:11:48 +0000

    built comment controller

--- app/controllers/api/v1/contact_controller.rb
+++ app/controllers/api/v1/contact_controller.rb
@@ -1,54 +1,52 @@
-class Api::V1::ContactController < ApplicationController
-
+class Api::V1::CommentsController < ApplicationController
 
     def index
-        @contact = Contact.all 
-        render json: @contact
+        @comment = Comment.all 
+        render json: @comment
     end
 
     def show
-        @contact = Contact.find_by(id: params[:id])
-        if @contact
-            render json: @contact
+        @comment = Comment.find_by(id: params[:id])
+        if @comment
+            render json: @comment
         else
-            render json: {error: 'contact not found.'}, status: 400
+            render json: {error: 'comment not found.'}, status: 400
         end
     end
 
     def create
-        @contact = Contact.new(contact_params)
-        if @contact.save
-            render json: @contact
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            render json: @comment
         else
-            render json: {error: 'Unable to create contact.'}, status: 400
+            render json: {error: 'Unable to create comment.'}, status: 400
         end
     end
 
     def destroy
-        @contact = Contact.find_by(id: params[:id])
-        if @contact
-        @contact.destroy()
-            render json: {message: 'contact has been deleated!'}
+        @comment = Comment.find_by(id: params[:id])
+        if @comment
+        @comment.destroy()
+            render json: {message: 'comment has been deleated!'}
         else
-            render json: {error: 'contact not found.'}, status: 400
+            render json: {error: 'comment not found.'}, status: 400
         end
     end
 
     def update
-        @contact = Contact.find_by(id: params[:id])
-        if @contact.update(contact_params)
-            render json: @contact
+        @comment = Comment.find_by(id: params[:id])
+        if @comment.update(comment_params)
+            render json: @comment
         else
-            render json: {error: 'Unable to update contact.'}, status: 400
+            render json: {error: 'Unable to update comment.'}, status: 400
         end
     end
 
     private
 
 
-    def contact_params
-        params.require(:contact).permit(:name, :subject, :email, :context)
+    def comment_params
+        params.require(:comment).permit(:content)
     end
-
 
 end
